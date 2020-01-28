6213e11e5c8e1b26310cd8375c1bdabe8475f847
Georgi Alexandrov <georgi@georgialexandrov.eu>
2020-01-28 22:30:12 +0200

    create-react-app files

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const app = require('express')();
+const app = require('express')()
+const apollo = require('apollo-server-express')
 
-// TODO: authentication
+const resolvers = require('./schema/resolvers')
+const schema = require('./schema/schema')
+// TODO: authentication. no worryes it's in local network
 
-app.use('/tv', require('./tvControl/routes'));
+app.use('/tv', require('./controls/tv/routes'))
+app.use('/thermostat', require('./controls/thermostat/routes'))
 
-app.listen(process.env.PORT || 3000, () => console.log('Smart home app listening on port 3000!'))
+const apolloServer = new apollo.ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+apolloServer.applyMiddleware({ app, path: '/graphql' })
+
+app.listen(process.env.PORT || 3000, () => console.log(`Smart home app listening on port ${process.env.PORT || 3000}!`))
